5413bfde3996078e234f99ec112da53d808afdf5
charlottestewart <costewart4@gmail.com>
2020-03-30 10:20:45 -0700

    changed Connection.php

--- app/Model/Connection.php
+++ app/Model/Connection.php
@@ -7,7 +7,7 @@ class Connection
         $dbhost = "localhost";
         $dbuser = "root";
         $dbpass = "root";
-        $db = "pms";
+        $db = "HousingHelper";
         $conn = new mysqli($dbhost, $dbuser,
             $dbpass, $db) or die("Connect failed: %s\n" .
             $conn->error);
