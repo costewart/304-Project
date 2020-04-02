1a8118061162a41b539d6b79fb85c57421f2cbb0
charlottestewart <costewart4@gmail.com>
2020-04-02 11:54:22 -0700

    tenant page is looking a bit better style wise but still needs work

--- app/View/tenant/index.php
+++ app/View/tenant/index.php
@@ -2,13 +2,37 @@
 <html>
     <head>
         <script type="text/javascript" src="/js/appointment.js"></script>
-        <link rel="stylesheet" type="text/css" href="/css/common.css" media="screen"/>
+         <link rel="stylesheet" type="text/css" href="/css/common.css" media="screen"/>
+        <link rel="stylesheet" type="text/css" href="/css/style.css">
     </head>
     <body>
         <?php $activeTab = 1;
         include('../app/View/header.php');  ?>
+
+<h1>Insert New Tenant</h1>
+<form class="form-char" name="form" method="post" action="/Tenant/action"> 
+
+<div class="input-group">
+<label> Tenant ID </label>
+<input type="text" name="id" placeholder="Enter ID" required />
+</div>
+
+<div class="input-group">
+<label> Name </label>
+<input type="text" name="pname" placeholder="Enter Name" required />
+</div>
+
+<div class="input-group">
+<label> Phone Number </label>
+<input type="text" name="phonenum" placeholder="Enter Phone Number" required />
+</div>
+
+<div class="input-group">
+<button class="btn-char" name="submit" type="submit">Add Tenant</button>
+</div>
+</form>
         <h1>Tenants</h1>
-        <table class="units-table">
+        <table class="tenant-table">
             <thead>
                 <tr>
                     <th>Name </th>
@@ -22,11 +46,13 @@
                 if (is_array($data) || is_object($data)) {
                 foreach ($data["tenants"] as $key => $tenant) {
                     echo "<tr><form method='post' action='/Tenant/actionTwo'>";
-                    echo "<td><input type=text name='pname' value='".$tenant["Name"]."'</td>";
-                    echo "<td><input type=text name='phonenum' value='".$tenant['PhoneNum']."'</td>";
-                    echo "<td><input type=text name='id' value='".$tenant['TenantID']."'</td>";
+     
+                    echo "<td><div class='input-group'><input type=text name='pname' value='".$tenant["Name"]."'</div></td>";
+                    echo "<td><div class='input-group'><input type=text name='phonenum' value='".$tenant['PhoneNum']."'</div></td>";
+                    echo "<td><div class='input-group'><input type=text name='id' value='".$tenant['TenantID']."'</div></td>";
                    // echo "<input type=hidden name=tid value='".$tenant['TenantID']."'>";
-                    echo "<td><input type=submit value='Update'>";
+                 
+                    echo "<td><div class='input-group'><button class='btn-char' name='submit' type=submit>Update</button></div></td>";
                     echo "</form></tr>";
                 }
             }
@@ -35,24 +61,7 @@
             </tbody>
         </table>
 
-        <div class="form">
-<div>
-<h1>Insert New Tenant</h1>
-<form name="form" method="post" action="/Tenant/action"> 
-<label> Tenant ID </label>
+    
 
-<input type="text" name="id" placeholder="Enter ID" required />
-
-<label> Name </label>
-
-<input type="text" name="pname" placeholder="Enter Name" required />
-<label> Phone Number </label>
-<input type="text" name="phonenum" placeholder="Enter Phone Number" required />
-<p><input name="submit" type="submit" value="Submit" /></p>
-</form>
-<p style="color:#FF0000;"></p>
-
-</div>
-</div>
     </body>
 </html>
