66728ee8c9e3406fb7691c35f2cdc795de860b83
mandy169 <mandysu169@gmail.com>
2020-04-03 13:10:46 -0700

    division fixed- Done

--- app/View/owner/index.php
+++ app/View/owner/index.php
@@ -8,17 +8,17 @@
         <?php $activeTab = 2;
         include('../app/View/header.php');  ?>
         <h1>Owners Listing</h1>
-        <form class="filter-table" action='/Owner/OwnersAll' method="post">
+        <form class="filter-table" action='/Owner/OwnersAll' method="post" style="max-width: 250px;">
             <!-- ACKNOWLEDGEMENT: code snippet for re-populating form values is from
                 https://stackoverflow.com/questions/5198304/how-to-keep-form-values-after-post -->
-            <div class="filter-label" >Owners who have all unit type of house or apartment</div>
+            <div class="filter-label" >Owners who have all unit types of units in the system.</div>
             <div class="filter-option">
-                <input type="radio" id="type-house" name="unit_type" value="house"
-                    <?php if(isset($_POST['unit_type']) && $_POST['unit_type'] == 'house' ) echo "checked='checked'"; ?> >
-                <label for="type-house">House</label> <br>
-                <input type="radio" id="type-apt" name="unit_type" value="apartment"
-                    <?php if(isset($_POST['unit_type']) && $_POST['unit_type'] == 'apartment') echo "checked='checked'"; ?> >
-                <label for="type-apt">Apartment</label>
+                <input type="radio" id="type-house" name="all_unit_types" value="true"
+                    <?php if(isset($_POST['all_unit_types']) && $_POST['all_unit_types'] == "true" ) echo "checked='checked'"; ?> >
+                <label for="type-house">All Unit Types</label> <br>
+                <input type="radio" id="type-apt" name="all_unit_types" value="false"
+                    <?php if(isset($_POST['all_unit_types']) && $_POST['all_unit_types'] == "false") echo "checked='checked'"; ?> >
+                <label for="type-apt">Not all unit types</label>
             </div>
             <div class="button-wrapper">
                 <input type="submit" value="Apply">
