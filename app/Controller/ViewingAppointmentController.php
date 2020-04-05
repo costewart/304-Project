c98dcb9228028a459e44f0112b9f75d58b3fe8cc
mandy169 <mandysu169@gmail.com>
2020-04-05 00:33:05 -0700

    Join Query Bug Fix- Data time format issue

--- app/Controller/ViewingAppointmentController.php
+++ app/Controller/ViewingAppointmentController.php
@@ -19,12 +19,12 @@ class ViewingAppointmentController extends Controller
     public function action() {
         $name = isset($_POST['property_manager_name']) ? $_POST['property_manager_name'] : "";
         if (isset($_POST['start_time'])) {
-            $start_time = date('d-m-Y h:i', strtotime($_POST['start_time']));
+            $start_time = date('Y-m-d h:i', strtotime($_POST['start_time']));
         } else {
             $start_time = "";
         }
         if (isset($_POST['end_time'])) {
-            $end_time = date('d-m-Y h:i', strtotime($_POST['end_time']));
+            $end_time = date('Y-m-d h:i', strtotime($_POST['end_time']));
         } else {
             $end_time = "";
         }
